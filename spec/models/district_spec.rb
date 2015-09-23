7ab3bd0b3a2e6cbe0fe2477fbdf7a91fb462afae
etiennebaque <etiennebaque@gmail.com>
2015-09-24 00:19:17 +0600

    Adjusted test related to district model

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -17,22 +17,4 @@ RSpec.describe District, :type => :model do
     expect(FactoryGirl.build(:district, name: nil)).not_to be_valid
   end
 
-  it 'is invalid without a latitude' do
-    expect(FactoryGirl.build(:district, latitude: nil)).not_to be_valid
-  end
-
-  it 'is invalid without a longitude' do
-    expect(FactoryGirl.build(:district, longitude: nil)).not_to be_valid
-  end
-
-  it 'has a latitude between -90 and 90' do
-    expect(FactoryGirl.build(:district, latitude: -100)).not_to be_valid
-    expect(FactoryGirl.build(:district, latitude: 100)).not_to be_valid
-  end
-
-  it 'has a longitude between -180 and 180' do
-    expect(FactoryGirl.build(:district, longitude: -190)).not_to be_valid
-    expect(FactoryGirl.build(:district, longitude: 190)).not_to be_valid
-  end
-
 end
