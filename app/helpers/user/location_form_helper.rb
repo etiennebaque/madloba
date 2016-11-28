e030fbf91c560e1c4c60126db3cb53bf953c3db9
Etienne Baque <etiennebaque@gmail.com>
2016-11-28 07:43:32 -0500

    Removed use of cache

--- app/helpers/user/location_form_helper.rb
+++ app/helpers/user/location_form_helper.rb
@@ -2,7 +2,7 @@ module User::LocationFormHelper
 
   # Get all the areas for the location form partial.
   def areas
-    Rails.cache.fetch(CACHE_AREAS) {Area.select(:id, :name, :latitude, :longitude)}
+    Area.all.select(:id, :name, :latitude, :longitude)
   end
 
 
